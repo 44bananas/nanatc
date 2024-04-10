ff92643e59b5b81a459da997fe7a428e176084f7
44bananas <59030706+44bananas@users.noreply.github.com>
2024-04-10 17:50:46 -0400

    updated parsing

--- db_to_api.py
+++ db_to_api.py
@@ -7,7 +7,6 @@ import base64
 
 app = Flask(__name__)
 
-
 @app.route("/tc")
 def wfm_rivens():
     dictionary = {}
@@ -17,8 +16,8 @@ def wfm_rivens():
     db = cur.execute("SELECT * FROM (SELECT * FROM rivens ORDER BY date DESC LIMIT 50) ORDER BY date DESC")
     db = db.fetchall()
     for i in range(len(db)):
-        dictionary.update({i:{"user":db[i][0],"weapon":db[i][1],"prefix":db[i][2],"price":db[i][9],"date":datetime.fromtimestamp(float(db[i][7])),"message":db[i][8],"first_stat":db[i][3],"second_stat":db[i][4],"third_stat":db[i][5],"neg_stat":db[i][6],"image":str(base64.b64decode(db[i][10]))}})
+        dictionary.update({i:{"user":db[i][0],"weapon":db[i][1],"prefix":db[i][2],"price":db[i][9],"date":datetime.fromtimestamp(float(db[i][7])),"message":db[i][8],"first_stat":db[i][3],"second_stat":db[i][4],"third_stat":db[i][5],"neg_stat":db[i][6],"image":str(base64.b64decode(db[i][10])),"neg_val":db[i][12],"first_stat_val":db[i][13],"second_stat_val":db[i][14],"third_stat_val":db[i][15],"raw_stat1":db[i][16],"raw_stat2":db[i][17],"raw_stat3":db[i][18],"raw_negstat":db[i][19]}})
     return dictionary
 
 while True:
-    app.run(host='ip goes here')
+    app.run(host='local host ip here')
